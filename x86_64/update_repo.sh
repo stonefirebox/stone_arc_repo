d182828dd95e95932c9e24ab1ce15b5a5c5b1f68
stonefirebox <stonefirebox@gmail.com>
2024-04-11 05:17:33 +0530

    add calamares and calamares-debug files

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,7 +1,7 @@
 rm stone_arc_repo*
 
 echo "repo-add"
-repo-add -n -R stone_arc_repo.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R stone_arc_repo.db.tar.gz *.pkg.tar.zst
 
 echo "####################################"
 echo "Repo Updated!!"
